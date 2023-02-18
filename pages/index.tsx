89261001326009d53afe785c92a089997fd07f19
Benj Ledesma <benj.ledesma@gmail.com>
2023-02-18 15:03:26 +0800

    Add "i use" section

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,11 +82,46 @@ const triedOut = [
   'other stuff'
 ]
 
+const uses: { url?: string; description: string; name: string }[] = [
+  {
+    name: 'MacBook Air M1, 2020 - 16GB RAM, 512GB SSD',
+    description: 'Lightweight, durable, and good enough to get the job done.'
+  },
+  {
+    name: 'Any 27" monitor',
+    description:
+      "The comfiest size for me. Any bigger and I'll have a lot of unused space, especially with dual monitors or ultrawides. Smaller sizes (even on just the MacBook) can work with smart window management."
+  },
+  {
+    name: 'NeoVim + tmux',
+    url: 'https://github.com/ledesmablt/dotfiles',
+    description:
+      "Thanks to these guys, I often only need 2 windows to work - my terminal and my browser. I'm only ever looking at one workspace at a time (great for focus), but switching to another project / repo or spinning up a background job can be done with only a few keystrokes."
+  },
+  {
+    name: 'Notion',
+    url: 'https://www.notion.so/',
+    description:
+      'Everything I need for project management, studying, note-taking, and so on. I love that the interface is mostly a clean black & white, putting full emphasis on the content itself and not the surrounding UI. And of course, the keyboard shortcuts are amazing.'
+  },
+  {
+    name: 'Google Calendar',
+    description:
+      "Pretty much anything that happens in my life gets calendared - meetings, workouts, chores, hangouts. Tasks always have a due date, whether it's next week or in 6 months. This helps remind me to do things, assess task urgency off the bat, and give quick answers regarding my availability."
+  },
+  {
+    name: 'Sennheiser MTW3',
+    url: 'https://www.sennheiser-hearing.com/en-US/p/momentum-true-wireless-3/',
+    description:
+      "The noise cancellation isn't as good as Sony's WF-1000XM4, but the sound quality is superb - as if you're wearing over-ear headphones. I may not make music as often anymore, but I sure love listening to it."
+  }
+]
+
 const Wrapper = styled('div')({
-  width: '100vw',
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center'
+  alignItems: 'center',
+  color: '#22292f'
 })
 
 const Container = styled('div')({
@@ -406,6 +441,37 @@ const Home: NextPage = ({ topTracks, lastRefreshed }: Props) => {
             <List>
               {triedOut.map((s, index) => (
                 <li key={`to-${index}`}>{s}</li>
+              ))}
+            </List>
+          </Section>
+
+          <Section>
+            <SectionHeader>{'i use'}</SectionHeader>
+            <List>
+              {uses.map(({ name, url, description }, index) => (
+                <li key={`uses-${index}`}>
+                  <div>
+                    {url ? (
+                      <a href={url} target='_blank' rel='noreferrer'>
+                        {name}
+                      </a>
+                    ) : (
+                      name
+                    )}
+                  </div>
+                  {description && (
+                    <div
+                      style={{
+                        lineHeight: 1.4,
+                        color: '#3d4852',
+                        marginTop: 6,
+                        marginBottom: 24
+                      }}
+                    >
+                      {description}
+                    </div>
+                  )}
+                </li>
               ))}
             </List>
           </Section>
