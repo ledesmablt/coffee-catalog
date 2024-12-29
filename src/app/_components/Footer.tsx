95542a0fce23da8d95073de42932d4fb2f98b2d4
Benj Ledesma <benj.ledesma@gmail.com>
2024-12-29 18:39:36 +0800

    chore: update footer text color

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export const Footer = () => {
   return (
-    <footer className='flex gap-2 mt-8'>
+    <footer className='flex gap-2 mt-8 text-zinc-600'>
       <Link href='https://ledesmablt.com'>(about the author)</Link>
     </footer>
   )
